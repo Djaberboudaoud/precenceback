52ba86e3a4d548246f2e86b4607317a0621d373d
Djaberboudaoud <boudaouddjaber123@gmail.com>
2026-05-20 20:55:31 +0100

    oss

--- main.py
+++ main.py
@@ -34,11 +34,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://localhost:5173",
-        "https://presence-1yz3.vercel.app",
-    ],
+    allow_origin_regex=".*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
